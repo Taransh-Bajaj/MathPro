e52b3b27389d0b3b368a7eb0ca1d4a477c178b2d
Taransh Bajaj <btaransh@gmail.com>
2026-02-21 22:23:29 +0530

    Render port fix

--- app.py
+++ app.py
@@ -39,4 +39,7 @@ def chat():
 # ---------- RUN ----------
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
-    
+    if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
